910edd3eff905504321346bf4718a3e6e1d30a4e
novak11 <novak11@IIT124>
2018-09-26 11:42:38 +0200

    auto sztaly kesz

--- 1_gyakorlat_auto/1_gyakorlat_auto/Program.cs
+++ 1_gyakorlat_auto/1_gyakorlat_auto/Program.cs
@@ -4,13 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _1_gyakorlat_auto
+namespace autonevter.futtathato
 {
     class Program
     {
         static void Main (string[] args)
         {
-
+            int darabszam = 2;
+            Auto[] auto = new Auto[darabszam];
+            auto[0] = new Auto ("Skoda", "superb", 2018, "1.8");
         }
     }
 }
